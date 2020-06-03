c3af3f53dec4bfdcf7356234fe324e5ee4557573
yuujiin <62110157+yuujiin@users.noreply.github.com>
2020-06-03 18:02:49 +0300

    Update README in header file

--- ILI9341/ILI9341_DMA_Driver.h
+++ ILI9341/ILI9341_DMA_Driver.h
@@ -36,48 +36,50 @@
 //	How to use this library
 //-----------------------------------
 //
-//	- Generate SPI peripheral and 3 GPIO outputs
-//		++ Cube code generation: separate .c/.h files for each peripheral
-//		++ SPI: CPOL=LOW, CPHA=1EDGE, 8 bits, MSB first, Software NSS, 50 MHz max
-//		++ DMA Tx for SPI: Half Word (16 bits) data width
-//		++ GPIO: GPIO_SPEED_FREQ_VERY_HIGH for CS and DC pins
-//		   Initial values: RST=Low, CS=High - LCD is in reset state before ILI_Init()
-//	- Configure parameters in ILI9341_Driver.h:
-//		++ If using MCUs other than STM32H7 you will have to change the #include "stm32h7xx_hal.h" to your respective .h file
-//		++ Define your ILI_HSPI_INSTANCE
-//		++ Define your CS, DC and RST outputs
-//		++ Check if ILI_SCREEN_WIDTH and ILI_SCREEN_HEIGHT match your LCD size and orientation.
-//		++ Check screen orientation
-//	- In your main program initialize LCD with ILI_Init(); before use
+//	* Generate SPI peripheral and 3 GPIO outputs
+//		* STM32Cube code generation: separate .c/.h files for each peripheral
+//		* SPI peripheral: CPOL=LOW, CPHA=1EDGE, 8 bits, MSB first, Software NSS, up to 50 MHz
+//		* Tx DMA for SPI: Half Word (16 bits) data width
+//		* GPIO: GPIO_SPEED_FREQ_VERY_HIGH for CS and DC pins
+//		* GPIO initial values: RST=Low, CS=High - LCD is in reset state before ILI_Init()
+//	* Configure parameters in ILI9341_Driver.h:
+//		* If using MCUs other than STM32H7 you will have to change the #include "stm32h7xx_hal.h" to your respective .h file
+//		* Define your ILI_HSPI_INSTANCE
+//		* Define your CS, DC and RST outputs
+//		* Check if ILI_SCREEN_WIDTH and ILI_SCREEN_HEIGHT match your LCD size and orientation.
+//	* In your main program initialize LCD with ILI_Init() before use
 //
-//	- Usage example:
-//		/* Includes */
-//		#include "ILI9341_DMA_Driver.h"
-//		/* Global vars */
-//		volatile uint16_t display_buf[ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT] __ALIGNED(32);	// aligned for DCache
-//		/* Interrupt callback */
-//		void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
-//			if (hspi == &ILI_HSPI_INSTANCE) {
-//				ILI_DMA_Callback();
-//			}
+//-----------------------------------
+//	Code example
+//-----------------------------------
+//
+//	/* Includes */
+//	#include "ILI9341_DMA_Driver.h"
+//	/* Global vars */
+//	volatile uint16_t display_buf[ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT] __ALIGNED(32);	// aligned for DCache
+//	/* Interrupt callback */
+//	void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
+//		if (hspi == &ILI_HSPI_INSTANCE) {
+//			ILI_DMA_Callback();
 //		}
-//		/* Main program */
-//		ILI_Init();
-//		/* Draw something */
-//		uint32_t i;
-//		for (i = 0; i < (ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT); i++) {
-//		  if (i < ILI_SCREEN_HEIGHT/4*ILI_SCREEN_WIDTH) {
-//			  display_buf[i] = RED;
-//		  } else if (i < ILI_SCREEN_HEIGHT/2*ILI_SCREEN_WIDTH) {
-//			  display_buf[i] = GREEN;
-//		  } else if (i < ILI_SCREEN_HEIGHT/4*3*ILI_SCREEN_WIDTH) {
-//			  display_buf[i] = BLUE;
-//		  } else {
-//			  display_buf[i] = YELLOW;
-//		  }
-//		}
-//		/* Load frame buffer */
-//		ILI_DMA_Load((uint16_t *)display_buf, (ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT));
+//	}
+//	/* Main program init section */
+//	ILI_Init();
+//	/* Draw something */
+//	uint32_t i;
+//	for (i = 0; i < (ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT); i++) {
+//	  if (i < ILI_SCREEN_HEIGHT/4*ILI_SCREEN_WIDTH) {
+//		  display_buf[i] = RED;
+//	  } else if (i < ILI_SCREEN_HEIGHT/2*ILI_SCREEN_WIDTH) {
+//		  display_buf[i] = GREEN;
+//	  } else if (i < ILI_SCREEN_HEIGHT/4*3*ILI_SCREEN_WIDTH) {
+//		  display_buf[i] = BLUE;
+//	  } else {
+//		  display_buf[i] = YELLOW;
+//	  }
+//	}
+//	/* Load frame buffer */
+//	ILI_DMA_Load((uint16_t *)display_buf, (ILI_SCREEN_WIDTH*ILI_SCREEN_HEIGHT));
 //
 //-----------------------------------
 
